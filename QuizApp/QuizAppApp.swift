f530b7f7302c244a1bf5cf42508a9a375875a31e
miugurlu <miugurlu@icloud.com>
2025-05-27 12:59:16 +0300

    Initial Commit

---
+++ QuizApp/QuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizAppApp.swift
+//  QuizApp
+//
+//  Created by İbrahim Uğurlu on 27.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct QuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
